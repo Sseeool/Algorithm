3b5e5bf68b244375faccbed698665cb1f8546eef
Sseeool <eonseo15@daum.net>
2023-10-07 17:37:30 +0900

    qsort func

--- 13step/ex08/sort_word_1181.c
+++ 13step/ex08/sort_word_1181.c
@@ -1,36 +1,3 @@
-/*#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-//비교 수행하는 함수, qsort의 4번째 인자
-int compare(const void* arg1, const void* arg2) 
-{   //길이순 정렬
-	if (strlen((const char*)arg1) > strlen((const char*)arg2)) return 1;
-	else if (strlen((const char*)arg1) < strlen((const char*)arg2)) return -1;
-	//길이 같으면 사전순
-	else return strcmp((char*)arg1, (char*)arg2);
-} 
-int main(void)
-{    
-	int size, length = 51;
-	char arr[20000][51] = { 0 };
-	scanf("%d", &size);
-
-	for (int i = 0; i < size; i++)      
-		scanf("%s", arr[i]);
-
-	//정렬
-	qsort(arr, size, sizeof(arr[0]), compare);
-	printf("\n\n----after sorting----\n\n");
-	//정렬 후 같은 문자열은 생략하고 출력
-	for (int i = 0; i < size; i++) {
-        printf("strcmp(arr[%d], arr[%d + 1]) = %d\n", i, i, strcmp(arr[i], arr[i + 1]));
-		if (strcmp(arr[i], arr[i+1]) != 0 || i == size - 1)
-			printf("%s\n", arr[i]);
-	}
-	return 0;
-}*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
